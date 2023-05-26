38ccf2245dd4072d66b42f53f3f16ee7d3bce7f9
MatthiasMu <132470395+MatthiasMu@users.noreply.github.com>
2023-05-26 09:46:09 +0200

    Update multiply.asm

--- SW/multiply.asm
+++ SW/multiply.asm
@@ -1,35 +1,46 @@
 	;LDIL - Load Immediate High
 	;LDIL - Load Immediate Low
 	
-	.org 0x100
-	.start
+		.org 0x0100
+		
+n1:		.data 15
+n2:		.data 25
 	
-	LDIL r0, 0 ;r0 wird auf 0 gesetzt
-	LDIH r0, 0x01 ;lade r0 mit der Speicheradresse 0x100
-	LD r1, [r0] ;erster Faktor in r1 laden
+
+result: .res 1 ;Reservier 1 Wort für das Ergebnis
+
+		.org 0x0000
+		.start
 	
-	LDIL r0, 0 ;zurücksetzen auf 0
-	LDIL r0, 0x01
-	LDIH r0, 0x01 ;r0 auf die Speicheradresse 0x101
-	
-	LD r2, [r0] ;zweite Zahl in r2
-	LDIL r0, 0;r0 wird auf 0 gesetzt
-	
-	LDIL r3, 0 ;Ergebnisregister 
-	LDIL r4, 1 ;Hilfsregister auf Wert 1
-	
-	LDIL r5, 0 ;Schleifenindex
+start:	XOR r0, r0, r0 ;r0 wird auf 0 gesetzt
+		LDIH r0, 0x0100
+		LDIL r0, n1 & 255
+		LDIH r0, n1 >> 8 
+		LD r1, [r0] ;erste Zahl in r1 laden
+		
+		XOR r0, r0, r0 ;zurücksetzen auf 0
+		LDIL r0, 0x0101
+		LDIL r0, n2 & 255
+		LDIH r0, n2 >> 8 
+		LD r2, [r0] ;zweite Zahl in r2
+		
+		XOR r0, r0, r0 ;r0 wird auf 0 gesetzt
+		
+		LDIL r3, 0 ;Ergebnisregister 
+		LDIL r4, 1 ;Hilfsregister auf Wert 1
+			
+		LDIL r5, loop & 255 ;r5 := loop
+		LDIH r5, loop >> 8
+		
 loop: ADD r3, r3, r1 ;Addiere r1 zu r3
 	  SUB r2, r2, r4 ;Subtrahiere 1 von r2 für den Schleifenindex
-	  JZ  r2, r5 ;wenn r2 -> 0: Springe auf Schleifenadresse
-	  
-	LDIL r0, 0x02
-	LDIH r0x 0x01 ;Lade r0 in 0x102
+	  JNZ r2, r5 ;wenn r2 nicht Null Springe auf r5 -> Loop
+	 
+	LDIH r0, 0x0102 ;Lade r0 in 0x0102
+	LDIL r0, result & 255
+	LDIH r0, result >> 8
 	ST [r0], r3 ;Ergebnis speichern
 	
 	HALT
-	
-	.org 0x100
-	result: .res 4 ;Reservier 4 Bytes für das Ergebnis
 	
 	.end
